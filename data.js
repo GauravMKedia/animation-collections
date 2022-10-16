5e0093b06d551285e097662b9bd377dcd0024e5a
pratikagrawal30 <115898632+pratikagrawal30@users.noreply.github.com>
2022-10-16 23:12:35 +0530

    Update data.js

--- data.js
+++ data.js
@@ -181,6 +181,12 @@ let animtaionData = [
         AnimationGif: 'https://i.imgur.com/clA8zge.gif',
         AnimationURL: "./animations/Hen/hen.html",
     },
+    {
+        ContributorName: 'Pratik Agrawal',
+        ContributorURL: "https://github.com/RudraPratik30",
+        AnimationGif: 'https://i.imgur.com/XppZLWQ.gif',
+        AnimationURL: "./animations/Cat/Cat.html",
+    },
     
 ]
 
