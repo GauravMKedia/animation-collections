35e1380ae37293e6a1744486e91fa43e2e8f1df6
coder-ritik <115949229+coder-ritik@users.noreply.github.com>
2022-10-18 09:22:23 +0530

    hay-card

--- data.js
+++ data.js
@@ -241,7 +241,7 @@ let animtaionData = [
         AnimationGif: "https://i.imgur.com/QK1G292.gif",
         AnimationURL: "/animations/code.divyo/code.divyo.html",
      },
-     {
+    {
         ContributorName: 'Ritik Sharma',
         ContributorURL: "https://github.com/coder-ritik",
         AnimationGif: 'https://i.imgur.com/GoXOhHo.gif',
@@ -252,6 +252,12 @@ let animtaionData = [
         ContributorURL: "https://github.com/patel-shivansh",
         AnimationGif: 'https://i.imgur.com/Lp1uMAI.gif',
         AnimationURL: "./animations/coffee-mug/coffee-mug.html",
+    },
+    {
+        ContributorName: 'Ritik Sharma',
+        ContributorURL: "https://github.com/coder-ritik",
+        AnimationGif: 'https://i.imgur.com/6LXslWv.gif',
+        AnimationURL: "./animations/hey-card/hey-card.html",
     },
 ]
 
